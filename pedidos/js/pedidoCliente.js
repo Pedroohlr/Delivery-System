312ac0eae8e72175062fbfdb8296dc20870a5377
pedroohlr <flygames1214@gmail.com>
2024-01-03 23:46:14 -0300

    Alteração para o cliente poder finalizar o proprio pedido

--- pedidos/js/pedidoCliente.js
+++ pedidos/js/pedidoCliente.js
@@ -8,10 +8,28 @@ function fazerPedido() {
     location.href = "../html/refeicao.html";
 };
 
+function confirmarCancelamento() {
+    document.querySelector('.float').innerHTML = `
+    <p>Deseja cancelar seu pedido?</p>
+    <br><br>
+    <button class="voltar">Não</button> <button class="cancelar">Sim</button>
+    `
+    document.querySelector('.float').style.display = 'block';
+    document.querySelector('.voltar').addEventListener('click', () => {
+        document.querySelector('.float').style.display = 'none';
+    });
+    document.querySelector('.cancelar').addEventListener('click', () => {
+        localStorage.removeItem('infos');
+        document.querySelector('.float').style.display = 'none';
+        alert("Seu pedido foi cancelado!");
+        location.href = '../../src/html/index.html'
+    });
+
+}
 
 
-function verPedido(){
-    if (!localStorage.getItem('infos')){
+function verPedido() {
+    if (!localStorage.getItem('infos')) {
         document.querySelector('.fazerPedido').innerHTML = `
         <br>
         Você não possui nenhum pedido em andamento.
@@ -31,6 +49,7 @@ function verPedido(){
         <p>Bebida: ${infos.bebida}</p>
         <p id ="entrega">Taxa de entrega: <strong>R$${infos.entrega.toFixed(2)}</strong></p>
         <p>No total de: <strong>R$${infos.valorTotal.toFixed(2)}</strong></p>
+        <button onclick="confirmarCancelamento()">Cancelar pedido</button>
     `
     };
 };
