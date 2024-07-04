8569f4c602f06c0a983a88afa9c425f61c5ef396
pedroohlr <flygames1214@gmail.com>
2024-07-03 21:11:17 -0300

    Adicionando env.js ao .gitignore

--- env.js
+++
@@ -1,8 +0,0 @@
-window.env = {
-  API_KEY: "AIzaSyDsQ1RgkFXYBDeOzgzEMoRZ9myy4USd5do",
-  authDomain: "lanches-e2650.firebaseapp.com",
-  projectId: "lanches-e2650",
-  storageBucket: "lanches-e2650.appspot.com",
-  messagingSenderId: "93042439010",
-  appId: "1:93042439010:web:fd9c3b2aa96f5d55f06faf"
-};
